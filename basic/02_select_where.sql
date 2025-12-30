befee1ae1c015bea9879c8f97a846450db4ca4d5
satsatsat-day <satdaysatsat@naver.com>
2025-12-30 12:49:21 +0900

    Create 02_select_where.sql

---
+++ basic/02_select_where.sql
@@ -0,0 +1,30 @@
+-- 테이블 구조 확인
+SHOW COLUMNS FROM customer;
+
+-- 전체 데이터 조회
+SELECT * FROM cusotmer;
+
+-- 특정 열 조회
+SELECT customer_id, first_name, last_name
+FROM customer;
+
+-- 비교 연산자를 이용한 조회
+SELECT customer_id, first_name, last_name
+FROM customer
+WHERE store_id < 5;
+
+-- 논리 연산자를 이용한 조회
+SELECT * FROM city
+WHERE (country_id = 103 OR country_id = 86)
+  AND city IN ('Cheju', 'Sunnyvale', 'Dallas');
+
+-- NULL과 공백
+-- 1) NULL 데이터 조회
+SELECT * FROM address
+WHERE address2 IS NULL;
+-- 2) NULL 아닌 데이터 조회
+SELECT * FROM address
+WHERE address2 IS NOT NULL;
+-- 3) 공백 조회 (공백은 NULL이 아님)
+SELECT * FROM address
+WHERE address2 = '';
