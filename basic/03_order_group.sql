25c339980e1302c95483bd49f0c99b074f3d3b99
satsatsat-day <satdaysatsat@naver.com>
2025-12-31 12:10:35 +0900

    Update 03_order_group.sql

--- basic/03_order_group.sql
+++ basic/03_order_group.sql
@@ -40,3 +40,24 @@ SELECT DISTINCT special_features, rating
 FROM film;
 
 
+
+-- **연습**
+-- 프로그래머스 Lv.1: 여러 기준으로 정렬하기
+-- ORDER BY
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
+
+-- 프로그래머스 Lv.1: 이름에 el이 들어가는 동물 찾기
+-- LIKE + 와일드카드 %
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'DOG' AND NAME LIKE '%el%'
+ORDER BY NAME;
+
+-- 프로그래머스 Lv.2: 고양이와 개는 몇 마리 있을까
+-- GROUP BY
+SELECT ANIMAL_TYPE, COUNT(*) AS count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
